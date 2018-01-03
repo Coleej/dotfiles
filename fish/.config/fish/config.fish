7cd8bedfa595669f40c16850a47b5e196dabac6e
Cody Johnson <codyjohnson144@gmail.com>
2018-01-03 11:08:49 -0600

    mostly fish files

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,7 +1,6 @@
 # Configuration for virtualfish
 set -x VIRTUALFISH_DEFAULT_PYTHON "/usr/bin/python3"
-eval (python3 -m virtualfish)
-
-set -x PATH $PATH "/home/cjohnson/.local/bin"
+set -x PROJECT_HOME "~/Projects"
+eval (python3 -m virtualfish auto_activation projects)
 
 cd ~
---
+++ init_scripts/fish.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+cd ~
+
+# set up dirs
+if [ ! -d "~/.config" ]; then
+	mkdir -p "~/.config/fish/functions"
+fi
+
+if [ ! -d "~/.config/fish" ]; then
+	mkdir -p "~/.config/fish/functions"
+fi
+
+if [ ! -d "~/.config/fish/functions" ]; then
+	mkdir "~/.config/fish/functions"
+fi
