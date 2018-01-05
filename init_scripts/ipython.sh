b260e7414f61078bfbd4de0f256eb23063e4742d
Cody Johnson <codyjohnson144@gmail.com>
2018-01-05 10:29:41 -0600

    added work profile to ipython

--- init_scripts/ipython.sh
+++ init_scripts/ipython.sh
@@ -4,11 +4,24 @@ cd ~
 
 # make dirs
 if [ ! -d ~/.ipython ]; then
+	
 	echo "ipython and profile_default directories don't exist."
 	echo "creating both directories."
 	mkdir -p ~/.ipython/profile_default
-elif [ ! -d ~/.ipython/profile_default ]; then
-	echo "profile_default directories don't exist."
-	echo "creating directory."
-	mkdir ~/.ipython/profile_default
+	mkdir -p ~/.ipython/work
+
+else
+	
+	if [ ! -d ~/.ipython/profile_default ]; then
+		echo "profile_default directory doesn't exist."
+		echo "creating directory."
+		mkdir ~/.ipython/profile_default
+	fi
+
+	if [ ! -d ~/.ipython/work ]; then
+		echo "work directory doesn't exist."
+		echo "creating directory."
+		mkdir ~/.ipython/work
+	fi
+
 fi
