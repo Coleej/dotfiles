494fad5af93c77682b81787dec15047b27faa207
Cody Johnson <codyjohnson144@gmail.com>
2018-01-06 13:24:22 -0600

    ipython work startup script

--- init_scripts/ipython.sh
+++ init_scripts/ipython.sh
@@ -7,21 +7,21 @@ if [ ! -d ~/.ipython ]; then
 	
 	echo "ipython and profile_default directories don't exist."
 	echo "creating both directories."
-	mkdir -p ~/.ipython/profile_default
-	mkdir -p ~/.ipython/work
+	mkdir -p ~/.ipython/profile_default/startup
+	mkdir -p ~/.ipython/work/startup
 
 else
 	
 	if [ ! -d ~/.ipython/profile_default ]; then
 		echo "profile_default directory doesn't exist."
 		echo "creating directory."
-		mkdir ~/.ipython/profile_default
+		mkdir ~/.ipython/profile_default/startup
 	fi
 
 	if [ ! -d ~/.ipython/work ]; then
 		echo "work directory doesn't exist."
 		echo "creating directory."
-		mkdir ~/.ipython/work
+		mkdir ~/.ipython/work/startup
 	fi
 
 fi
