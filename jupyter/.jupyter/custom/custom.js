7ed0816d950782e12d3761418c94c27257c73aeb
Cody Johnson <codyjohnson144@gmail.com>
2018-01-18 09:07:12 -0600

    custom jupyter vim bindings

--- jupyter/.jupyter/custom/custom.js
+++ jupyter/.jupyter/custom/custom.js
@@ -2,7 +2,11 @@
 require([
   'nbextensions/vim_binding/vim_binding'
 ], function() {
-  // Map jj to <Esc>
+  // Map jk to <Esc>
   CodeMirror.Vim.map("jk", "<Esc>", "insert");
+  CodeMirror.Vim.map("jk", "<Esc>", "visual");
+  // Map H and L to beginning and end of line
+  CodeMirror.Vim.map("H", "0", "normal");
+  CodeMirror.Vim.map("L", "$", "normal");
 });
 
