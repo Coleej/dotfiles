4afee357d988bec9430944ecf5dce750d034124d
Cody Johnson <codyjohnson144@gmail.com>
2018-02-18 09:43:08 -0600

    added important_values.py to ipython startup scripts

---
+++ ipython/.ipython/profile_work/startup/02-important_values.py
@@ -0,0 +1,11 @@
+# caminada metrics
+cam1_vol = 2530677 * (ureg.m**3) # cam I volume of placement
+cam2_vol = 3897701 * (ureg.m**3) # cam II volume of placement
+cam_bch_z = 1.3716 * (ureg.m) # cam beach elevation NAVD88
+cam_bch_W = 19.8 * (ureg.m) # cam beach width
+cam_dune_z = (7 * ureg.ft).ito(ureg.m) # cam dune elevation NAVD88
+cam_dc_W = (290 * ureg.ft).ito(ureg.m) # cam dune crest width
+cam1_A = (303 * ureg.acre).ito(ureg.km**2) # cam 1 surface area
+cam2_A = (489 * ureg.acre).ito(ureg.km**2) # cam 2 surface area
+cam1_L = (31000 * ureg.ft).ito(ureg.km) # cam 1 length
+cam2_L = (39000 * ureg.ft).ito(ureg.km) # cam 2 length
