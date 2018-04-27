9fc838d17e4d5e75c08306c12ab512691a0b7c10
Cody Johnson <codyjohnson144@gmail.com>
2018-04-27 09:19:25 -0500

    modified sync_d3d

--- fish/.config/fish/functions/sync_d3d.fish
+++ fish/.config/fish/functions/sync_d3d.fish
@@ -1,3 +1,8 @@
 function sync_d3d
-  rsync -avr --no-l --delete --exclude-from=/home/cjohnson/.rsync_delft3d_excludes --filter='P *.dat' --filter='P *.def' cjoh296@qb.loni.org:/work/cjoh296/delft3d.wd/ /media/cjohnson/sg_external/model_results/delft3d
+  switch (hostname)
+  case "mobile"
+    rsync -avr --no-l --delete --exclude-from=/home/cjohnson/.rsync_delft3d_excludes --filter='P *.dat' --filter='P *.def' cjoh296@qb.loni.org:/work/cjoh296/delft3d.wd/ /media/cjohnson/sg_external/model_results/delft3d
+  case "dell-desk"
+    rsync -avr --no-l --delete --exclude-from=/home/cjohnson/.rsync_delft3d_excludes --filter='P *.dat' --filter='P *.def' cjoh296@qb.loni.org:/work/cjoh296/delft3d.wd/ /home/cjohnson/Modeling/delft3d
+  end
 end
